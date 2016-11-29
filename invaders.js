2c51841c51434ad4685ce5cd62302ff17f34bb45
Evan Bacon <baconbrix@gmail.com>
2016-11-29 16:50:18 -0600

    Its Gucci

--- invaders.js
+++ invaders.js
@@ -87,7 +87,9 @@ $(function() {
     enemyBullets.setAll('checkWorldBounds', true);
 
     //  The hero!
-    player = game.add.sprite(400, 500, 'ship');
+    player = game.add.sprite(game.width/2, game.height * 0.8, 'ship');
+    player.width = game.width * 0.1;
+    player.height = player.width;
     player.anchor.setTo(0.5, 0.5);
     game.physics.enable(player, Phaser.Physics.ARCADE);
 
@@ -104,7 +106,7 @@ $(function() {
 
     //  Lives
     lives = game.add.group();
-    game.add.text(game.world.width - 100, 10, 'Lives : ', { font: '34px Arial', fill: '#fff' });
+    game.add.text(game.world.width - 120, 10, 'Lives : ', { font: '34px Arial', fill: '#fff' });
 
     //  Text
     stateText = game.add.text(game.world.centerX,game.world.centerY,' ', { font: '84px Arial', fill: '#fff' });
@@ -204,6 +206,7 @@ $(function() {
 
     let config = configGen();
 
+    let size = game.width * 0.08;
     for (var row = 0; row < config.length; row++)
     {
       let rowVal = config[row];
@@ -212,7 +215,9 @@ $(function() {
         let colVal = rowVal[col];
 
         if (colVal == 0) continue;
-        var alien = aliens.create(col * 48, row * 50, 'invader');
+        var alien = aliens.create(col * size, row * size, 'invader');
+        alien.width = size;
+        alien.height = size;
         alien.anchor.setTo(0.5, 0.5);
         // alien.animations.add('fly', [ 0, 1, 2, 3 ], 20, true);
         game.add.tween(alien).to({ angle: 45 }, 2000, function(k) {                return Math.sin(Math.PI * 2 * k);            }, true, 0, -1);
@@ -221,11 +226,13 @@ $(function() {
       }
     }
 
-    aliens.x = 100;
+    let point = (game.width / 2) - (aliens.width/2);
+    console.log(aliens.width/2);
+    aliens.x = point;
     aliens.y = 50;
 
     //  All this does is basically start the invaders moving. Notice we're moving the Group they belong to, rather than the invaders directly.
-    var tween = game.add.tween(aliens).to( { x: 200 }, 2000, Phaser.Easing.Linear.None, true, 0, 1000, true);
+    var tween = game.add.tween(aliens).to( { x: point + size }, 2000, Phaser.Easing.Linear.None, true, 0, 1000, true);
 
     //  When the tween loops it calls descend
     tween.onLoop.add(descend, this);
@@ -245,6 +252,7 @@ $(function() {
 
   }
 
+  let lastDirection;
   function update() {
 
     //  Scroll the background
@@ -255,13 +263,35 @@ $(function() {
       //  Reset the player, then check for movement keys
       player.body.velocity.setTo(0, 0);
 
+      let speed = 200;
+      let rotationSpeed = 200;
+
+      if (game.input.pointer1.isDown) {
+        if (game.input.pointer1.x < game.width/2) {
+          lastDirection = -1;
+          player.body.velocity.x = -speed;
+          player.body.angularVelocity = -rotationSpeed;
+        } else if (game.input.pointer1.x > game.width/2) {
+          lastDirection = 1;
+          player.body.angularVelocity = rotationSpeed;
+
+          player.body.velocity.x = speed;
+        } else {
+          lastDirection = 0;
+          player.body.angularVelocity = 0;
+        }
+      } else {
+        player.body.angularVelocity = 0;
+
+      }
+
       if (cursors.left.isDown)
       {
-        player.body.velocity.x = -200;
+        player.body.velocity.x = -speed;
       }
       else if (cursors.right.isDown)
       {
-        player.body.velocity.x = 200;
+        player.body.velocity.x = speed;
       }
 
       //  Firing?
